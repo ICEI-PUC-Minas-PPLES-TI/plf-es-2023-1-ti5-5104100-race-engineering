ed433fe1779038d4c3a1484bd29c0833a02e5c9f
joeyclapton <joeyclapton42@gmail.com>
2023-04-09 22:32:48 -0300

    refactor: structure code

--- codigo/mobile/app/lib/pages/home/home.dart
+++ codigo/mobile/app/lib/pages/home/home.dart
@@ -3,15 +3,12 @@ import 'package:app/pages/home/components/footer.dart';
 import 'package:app/pages/home/components/header.dart';
 import 'package:flutter/cupertino.dart';
 
-
 class HomeView extends StatefulWidget {
   @override
   _HomeView createState() => _HomeView();
 }
 
 class _HomeView extends State<HomeView> {
-  int _selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -21,21 +18,13 @@ class _HomeView extends State<HomeView> {
           children: [
             Expanded(
                 flex: 5,
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Header()
-                )),
+                child: Padding(padding: const EdgeInsets.all(16), child: Header())),
             Expanded(
                 flex: 3,
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Body()
-                )),
-      Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Footer()
-      ),
-        ],
+                child: Padding(padding: const EdgeInsets.all(16), child: Body())),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16), child: Footer()),
+          ],
         ),
       ),
     );
