172d3136847f4436d49260eb351764ae66deb63d
ianmarcel <ianmarcelcampos@gmail.com>
2023-04-19 19:42:17 -0300

    criacão da weather app

--- codigo/web/race-engineering/next.config.js
+++ codigo/web/race-engineering/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+//Acrescentei essa parte abaixo
+module.exports = {
+  images: {
+    domains: ["images.unsplash.com", "openweathermap.org"],
+  },
+};
