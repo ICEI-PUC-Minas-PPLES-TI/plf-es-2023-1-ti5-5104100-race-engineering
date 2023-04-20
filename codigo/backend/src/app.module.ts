df90848ef99b8e23d4d0e350cc1600408ef0b837
Octávio Rocha <octaviorocha1199@gmail.com>
2023-04-20 17:18:20 -0300

    add logger to API

--- codigo/backend/src/app.module.ts
+++ codigo/backend/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getEnvPath } from './common/helper/env.helper';
 import { ApiModule } from './api/api.module';
+import { LoggerMiddleware } from '@/common/logger/logger.middleware';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
@@ -34,4 +35,8 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
