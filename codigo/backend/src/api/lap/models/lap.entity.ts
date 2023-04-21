49b5acd79d7d93f850f74d5f15faa7421987fc38
Octávio Rocha <octaviorocha1199@gmail.com>
2023-04-21 11:28:02 -0300

    lap entity and relation changes

--- codigo/backend/src/api/lap/models/lap.entity.ts
+++ codigo/backend/src/api/lap/models/lap.entity.ts
@@ -1,4 +1,5 @@
 import {
+  BaseEntity,
   Column,
   CreateDateColumn,
   DeleteDateColumn,
@@ -11,25 +12,27 @@ import {
 } from 'typeorm';
 import { Race } from '../../race/models/race.entity';
 import { Exclude } from 'class-transformer';
+import { Driver } from '@/api/driver/models/driver.entity';
 
 @Index('Lap_pkey', ['id'], { unique: true })
 @Entity('Lap', { schema: 'public' })
-export class Lap {
+export class Lap extends BaseEntity {
   @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
   id: number;
 
-  @Column('numeric', { name: 'bestTime', nullable: true })
-  bestTime: number | null;
+  @Column('numeric', { name: 'lapTime', nullable: true })
+  lapTime: number | null;
 
-  @Column('numeric', { name: 'averageTime', nullable: true })
-  averageTime: number | null;
-
-  @Column('numeric', { name: 'totalTime', nullable: true })
-  totalTime: number | null;
+  @Column('numeric', { name: 'lapNumber', nullable: true })
+  lapNumber: number | null;
 
   @ManyToOne(() => Race, (race) => race.laps)
   @JoinColumn([{ name: 'raceId', referencedColumnName: 'id' }])
   race: Race;
+
+  @ManyToOne(() => Driver, (driver) => driver.laps)
+  @JoinColumn([{ name: 'driverId', referencedColumnName: 'id' }])
+  driver: Driver;
 
   @Exclude()
   @CreateDateColumn({ name: 'createdAt', type: 'timestamp', default: 'now()' })
