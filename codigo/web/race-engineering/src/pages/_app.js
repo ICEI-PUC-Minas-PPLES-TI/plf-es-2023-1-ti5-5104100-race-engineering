20e95a3661f78186290f4545ad964830102935bb
joeyclapton <joeyclapton42@gmail.com>
2023-04-25 13:58:36 -0300

    feat: add login like first page

--- codigo/web/race-engineering/src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../pages/home/index.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- codigo/web/race-engineering/src/pages/index.tsx
+++ codigo/web/race-engineering/src/pages/index.tsx
@@ -1,55 +1,37 @@
-import { Inter } from "next/font/google";
-import Head from "next/head";
-//import Image from 'next/image';
-import NextLink from "next/link";
-
 import Sidebar from "@/components/sidebar/Sidebar";
-//import Layout  from '@/components/Layout';
-//import Hero from '@/components/Hero';
-import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Center,
-  Heading,
-  HStack,
-  Image,
-  Link,
-  Stack,
-  Tag,
-} from "@chakra-ui/react";
+import { useAuth } from "@/context/AuthContext";
+import { Box } from "@chakra-ui/react";
 
-import Logo from "./home/footer";
 import Home from "./home/index";
 import LoginPage from "./login";
 
 export default function Index() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <>
-      {/* <Logo /> */}
+      {!isAuthenticated ? (
+        <LoginPage />
+      ) : (
+        <Box height="100vh" width="100%">
+          <Box
+            height="100%"
+            width="100%"
+            display="flex"
+            justifyContent="center"
+            flex-flexDirection="column"
+            alignItems="center"
+          >
+            <Box w="5vw" className="sidebar-container">
+              <Sidebar />
+            </Box>
 
-      {/* <Hero /> */}
-
-      <Box height="100vh" width="100%">
-        <Box
-          height="100%"
-          width="100%"
-          display="flex"
-          justifyContent="center"
-          flex-flexDirection="column"
-          alignItems="center"
-        >
-          <Box w="5vw" className="sidebar-container">
-            <Sidebar />
-          </Box>
-
-          <Box w="95vw">
-            <Home />
+            <Box w="95vw">
+              <Home />
+            </Box>
           </Box>
         </Box>
-      </Box>
+      )}
     </>
   );
 }
