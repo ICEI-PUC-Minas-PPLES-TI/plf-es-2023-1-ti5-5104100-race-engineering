7a0eecedb37542eb1e30fe1ee517cda70886a453
Vítor José <vitorjoselara@gmail.com>
2023-05-01 23:27:56 -0300

    Ajuste na responsividade em diferentes navegadores

--- codigo/web/race-engineering/src/pages/home/index.tsx
+++ codigo/web/race-engineering/src/pages/home/index.tsx
@@ -24,162 +24,161 @@ export default function Home() {
 
   return (
     <Box height="100vh" width="100%" padding="0 100px">
-      <Heading as="h1" size="2xl" textAlign="center" marginTop="2%" marginBottom="5%">
-        Gerenciamento Porshce Cup
+      <Heading as="h1" size="2xl" textAlign="center" marginTop="3%" marginBottom="5%">
+        Gerenciamento Porsche Cup
       </Heading>
 
-      <SimpleGrid
-        spacing={10}
-        templateColumns="repeat(auto-fill, minmax(350px, 1fr))"
-        margin="-15"
-        padding="-15"
-        marginLeft="10"
-        marginRight="10"
-        marginTop="10"
-        marginBottom="10"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Card
-          align="center"
-          backgroundImage="./images/gradient-black.png"
-          maxHeight="250px"
+      <Box padding="0 20px" margin="0 auto">
+        <SimpleGrid
+          spacing={10}
+          templateColumns="repeat(3, 1fr)"
+          justifyContent="center"
+          alignItems="center"
         >
-          <CardHeader>
-            <Heading color="#ffffff" size="md">
-              Corridas
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Text
-              color="#ffffff"
-              align="center"
-              lineHeight="normal"
-              fontSize={15}
-            >
-              Gerencie corridas com facilidade: liste, crie, edite ou remova
-              corridas conforme necessário.
-            </Text>
-          </CardBody>
-          <CardFooter w="100%">
-            <Button
-              variant="solid"
-              onClick={() => {
-                router.push("/list-race");
-              }}
-              colorScheme="whiteAlpha"
-              w="50%"
-              mr="2"
-            >
-              Listar
-            </Button>
-            <Button
-              variant="solid"
-              onClick={() => {
-                // router.push("/register-user");
-                router.push("/register-race");
-              }}
-              colorScheme="whiteAlpha"
-              w="50%"
-              ml="2"
-            >
-              Cadastrar
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card
-          align="center"
-          backgroundImage="./images/gradient-red.png"
-          maxHeight="250px"
-        >
-          <CardHeader>
-            <Heading color="#ffffff" size="md">
-              Circuitos
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Text
-              color="#ffffff"
-              align="center"
-              lineHeight="normal"
-              fontSize={15}
-            >
-              Gerencie os circuitos com facilidade: liste, crie, edite ou remova
-              corridas conforme necessário.
-            </Text>
-          </CardBody>
-          <CardFooter w="100%">
-            <Button
-              variant="solid"
-              onClick={() => {
-                router.push("/list-circuit");
-              }}
-              colorScheme="whiteAlpha"
-              w="50%"
-              mr="2"
-            >
-              Listar
-            </Button>
-            <Button
-              variant="solid"
-              onClick={() => {
-                router.push("/create-circuit");
-              }}
-              colorScheme="whiteAlpha"
-              w="50%"
-              ml="2"
-            >
-              Cadastrar
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card
-          align="center"
-          backgroundImage="./images/gradient-green.png"
-          maxHeight="250px"
-        >
-          <CardHeader>
-            <Heading color="#ffffff" size="md">
-              Times
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Text
-              color="#ffffff"
-              align="center"
-              lineHeight="normal"
-              fontSize={15}
-            >
-              Gerencie os times com facilidade: liste, crie, edite ou remova
-              times conforme necessário.
-            </Text>
-          </CardBody>
-          <CardFooter w="100%">
-            <Button
-              variant="solid"
-              onClick={() => {
-                router.push("/list-team");
-              }}
-              colorScheme="whiteAlpha"
-              w="50%"
-              mr="2"
-            >
-              Listar
-            </Button>
-            <Button
-              variant="solid"
-              onClick={() => {
-                router.push("/create-team");
-              }}
-              colorScheme="whiteAlpha"
-              w="50%"
-              ml="2"
-            >
-              Cadastrar
-            </Button>
-          </CardFooter>
-        </Card>
-      </SimpleGrid>
+          <Card
+            align="center"
+            backgroundImage="./images/gradient-black.png"
+            maxHeight="250px"
+            width="100%"
+          >
+            <CardHeader>
+              <Heading color="#ffffff" size="md">
+                Corridas
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text
+                color="#ffffff"
+                align="center"
+                lineHeight="normal"
+                fontSize={15}
+              >
+                Gerencie corridas com facilidade: liste, crie, edite ou remova
+                corridas conforme necessário.
+              </Text>
+            </CardBody>
+            <CardFooter w="100%">
+              <Button
+                variant="solid"
+                onClick={() => {
+                  router.push("/list-race");
+                }}
+                colorScheme="whiteAlpha"
+                w="50%"
+                mr="2"
+              >
+                Listar
+              </Button>
+              <Button
+                variant="solid"
+                onClick={() => {
+                  // router.push("/register-user");
+                  router.push("/register-race");
+                }}
+                colorScheme="whiteAlpha"
+                w="50%"
+                ml="2"
+              >
+                Cadastrar
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card
+            align="center"
+            backgroundImage="./images/gradient-red.png"
+            maxHeight="250px"
+            width="100%"
+          >
+            <CardHeader>
+              <Heading color="#ffffff" size="md">
+                Circuitos
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text
+                color="#ffffff"
+                align="center"
+                lineHeight="normal"
+                fontSize={15}
+              >
+                Gerencie os circuitos com facilidade: liste, crie, edite ou remova
+                corridas conforme necessário.
+              </Text>
+            </CardBody>
+            <CardFooter w="100%">
+              <Button
+                variant="solid"
+                onClick={() => {
+                  router.push("/list-circuit");
+                }}
+                colorScheme="whiteAlpha"
+                w="50%"
+                mr="2"
+              >
+                Listar
+              </Button>
+              <Button
+                variant="solid"
+                onClick={() => {
+                  router.push("/create-circuit");
+                }}
+                colorScheme="whiteAlpha"
+                w="50%"
+                ml="2"
+              >
+                Cadastrar
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card
+            align="center"
+            backgroundImage="./images/gradient-green.png"
+            maxHeight="250px"
+            width="100%"
+          >
+            <CardHeader>
+              <Heading color="#ffffff" size="md">
+                Times
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text
+                color="#ffffff"
+                align="center"
+                lineHeight="normal"
+                fontSize={15}
+              >
+                Gerencie os times com facilidade: liste, crie, edite ou remova
+                times conforme necessário.
+              </Text>
+            </CardBody>
+            <CardFooter w="100%">
+              <Button
+                variant="solid"
+                onClick={() => {
+                  router.push("/list-team");
+                }}
+                colorScheme="whiteAlpha"
+                w="50%"
+                mr="2"
+              >
+                Listar
+              </Button>
+              <Button
+                variant="solid"
+                onClick={() => {
+                  router.push("/create-team");
+                }}
+                colorScheme="whiteAlpha"
+                w="50%"
+                ml="2"
+              >
+                Cadastrar
+              </Button>
+            </CardFooter>
+          </Card>
+        </SimpleGrid>
+      </Box>
     </Box>
   );
 }
