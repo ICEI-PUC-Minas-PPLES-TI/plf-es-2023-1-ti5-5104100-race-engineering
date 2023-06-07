4b22ca38fbb343a233409667141bfcb845be7512
joeyclapton <joeyclapton42@gmail.com>
2023-06-07 00:34:04 -0300

    feat: add new api url

--- codigo/web/race-engineering/src/services/api.ts
+++ codigo/web/race-engineering/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://race-engineering-api.azurewebsites.net/api",
 });
 
 api.defaults.timeout = 10000000;
